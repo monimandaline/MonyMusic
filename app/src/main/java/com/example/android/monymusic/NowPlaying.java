335a065fa38c78350a0e16cf2843804003fd287a
Csontos Mónika <csm@mandaline.hu>
2017-05-02 15:27:27 +0200

    Player button animations

--- app/src/main/java/com/example/android/monymusic/NowPlaying.java
+++ app/src/main/java/com/example/android/monymusic/NowPlaying.java
@@ -5,12 +5,26 @@ package com.example.android.monymusic;
  */
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.android.monymusic.R.id.menu1_textview;
+import static com.example.android.monymusic.R.id.menu2_textview;
+import static com.example.android.monymusic.R.id.menu3_textview;
+import static com.example.android.monymusic.R.id.menu4_textview;
+import static com.example.android.monymusic.R.id.menu5_textview;
+import static com.example.android.monymusic.R.id.menu6_textview;
 
 
 public class NowPlaying extends AppCompatActivity  {
@@ -55,6 +69,68 @@ public class NowPlaying extends AppCompatActivity  {
         MyMenu NewMyMenu = new MyMenu();
         NewMyMenu.MenuHandler(v);
     }
+
+
+    public void PlayerButtons(View v)
+    {
+        int getid = v.getId();
+
+        // each click starts the proper intent
+        switch (getid) {
+            case R.id.player_button1:
+                ImageView stopImageView = (ImageView) findViewById(R.id.player_button1);
+                stopImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ButtonAnimation(v);
+                        Toast.makeText(NowPlaying.this, "Stop!", Toast.LENGTH_SHORT).show();}
+                });
+                break;
+            case R.id.player_button2:
+                ImageView stopImageView2 = (ImageView) findViewById(R.id.player_button2);
+                stopImageView2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ButtonAnimation(v);
+                        Toast.makeText(NowPlaying.this, "Back!", Toast.LENGTH_SHORT).show();}
+                });
+                break;
+            case R.id.player_button3:
+                ImageView stopImageView3 = (ImageView) findViewById(R.id.player_button3);
+                stopImageView3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ButtonAnimation(v);
+                        Toast.makeText(NowPlaying.this, "Play!", Toast.LENGTH_SHORT).show();}
+                });
+                break;
+            case R.id.player_button4:
+                ImageView stopImageView4 = (ImageView) findViewById(R.id.player_button4);
+                stopImageView4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ButtonAnimation(v);
+                        Toast.makeText(NowPlaying.this, "Forward!", Toast.LENGTH_SHORT).show();}
+                });
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+
+    // Button animations
+    public void ButtonAnimation(View v) {
+
+        final Animation animTranslate = AnimationUtils.loadAnimation(this, R.anim.anim_translate);
+
+        AnimationSet sets = new AnimationSet(false);
+        sets.addAnimation(animTranslate);
+        v.startAnimation(sets);
+    }
+
 
 }
 
