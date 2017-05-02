673e5b1fc09345c2635db2b1400c40fc29eec75c
Csontos Mónika <csm@mandaline.hu>
2017-05-02 09:30:59 +0200

    all menu ok, pop up, styles ok, dimens ok, colors ok, strings ok, background pic ok comments ok (problem: scroll menu - not one object, too many rows code is repeated into all java class)

--- app/src/main/java/com/example/android/monymusic/Menu.java
+++ app/src/main/java/com/example/android/monymusic/Menu.java
@@ -1,55 +1,18 @@
 package com.example.android.monymusic;
 
-/**
- * Created by Csontos Mónika on 2017. 04. 29..
- */
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * Created by Csontos Mónika on 2017. 05. 01..
+ */
 
-public class NowPlaying extends AppCompatActivity  {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.v("NowPlaying", "click");
-
-        // Global variable for Help popup window, where you can find a description for actual screen
-        // The menu number allows for Pop.java code to select the correct text
-        MainActivity.menu = 1;
-
-        setContentView(R.layout.nowplaying);
-   }
-
-    @Override
-    public void onResume ()
-    { // This code handle the local menu on the top of screen
-        super.onResume();
-        TextView menu1_textview = (TextView) findViewById(R.id.scroll_menu1);
-        TextView menu2_textview = (TextView) findViewById(R.id.scroll_menu2);
-        TextView menu3_textview = (TextView) findViewById(R.id.scroll_menu3);
-        TextView menu4_textview = (TextView) findViewById(R.id.scroll_menu4);
-        TextView menu5_textview = (TextView) findViewById(R.id.scroll_menu5);
-        TextView menu6_textview = (TextView) findViewById(R.id.scroll_menu6);
-
-        menu1_textview.setTextColor(Color.parseColor("#FFFFFF"));
-        menu2_textview.setTextColor(Color.parseColor("#FFFFFF"));
-        menu3_textview.setTextColor(Color.parseColor("#FFFFFF"));
-        menu4_textview.setTextColor(Color.parseColor("#FFFFFF"));
-        menu5_textview.setTextColor(Color.parseColor("#FFFFFF"));
-        menu6_textview.setTextColor(Color.parseColor("#FFFFFF"));
-
-        // Global variable for Help popup window, where you can find a description for actual screen
-        // The menu number allows for Pop.java code to select the correct text
-        MainActivity.menu = 1;
-    }
-
-    public void ScrollMenu(View v) {
+// here is the problem.....................................
+public class Menu  extends AppCompatActivity {
         // This code manages the local menu on top of the screen
         Intent MonyMusic;
         TextView menu1_textview = (TextView) findViewById(R.id.scroll_menu1);
@@ -105,7 +68,7 @@ public class NowPlaying extends AppCompatActivity  {
             case R.id.scroll_menu6:
                 Log.v("NowPlaying", "click 6");
                 menu6_textview.setTextColor(Color.parseColor("#66000000"));
-                startActivity(new Intent(NowPlaying.this, Pop.class));
+                startActivity(new Intent(Menu.this, Pop.class));
                 menu6_textview.setTextColor(Color.parseColor("#000000"));
 
                 break;
@@ -114,9 +77,3 @@ public class NowPlaying extends AppCompatActivity  {
         }
 
     }
-
-
-
-
-}
-
